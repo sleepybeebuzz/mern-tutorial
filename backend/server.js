5172c720d827e454537bd0343b9ca08722cda24a
brianhuyphung <brianhuyphung@gmail.com>
2022-08-27 22:02:27 -0700

    yo

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+//backend web framework here
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/goals', (req, res) => {
+    res.send('Get goals')
+})
+
+app.listen(port, () => console.log('Server started on port ${port}'))
